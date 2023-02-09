7f8b3f3b064774d7871f26f7e3ec296062478ce8
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-02-09 23:53:56 +0500

    deleted mailer

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,71 +2,61 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\GuestContactUsRequest;
 use App\Models\User;
-use App\Models\UserContactUsRequest;
+use App\Mail\contctReqt;
 use Illuminate\Http\Request;
-use Illuminate\Mail\Mailer;
+use App\Models\UserContactUsRequest;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Mail;
+use App\Models\GuestContactUsRequest;
 
 class MailController extends Controller
 {
     public function sendRequestToAdmin(Request $request)
     {
-        if (Auth::user()) {
-            $user = new UserContactUsRequest();
-            $user->userId = Auth::user()->id;
-            $user->messageBox = $request->messageBox;
-            $user->save();
-            $name = $request->name;
-            $email = $request->email;
-            $messageBox = "<h4 style='color:red;'>Contact Us Request From User .</h4>" . "<hr>" . $request->messageBox;
-            $adminEmail = env('MAIL_ADMIN_EMAIL_ADDRESS');
-            $mail = new Mailer();
-            $mail->IsSMTP();
-            $mail->setFrom($mail->Username);
-            $mail->AddReplyTo($email, $name);
-            $mail->AddAddress($adminEmail);
-            $mail->IsHTML(true);
-            $mail->Subject = "Conatact Us Request";
-            $mail->Body = $messageBox;
-            try {
-                $mail->send();
-            } catch (Exception $ex) {
-                return "Email Blocked Error Please Try Again Later ";
-            }
-            return redirect()->back()->with('errorMessage', 'Your Request Submited SuccessFully Our Team Will Contact You In 24 Hours')->with('className', 'success');
-        } else {
-            $guest = new GuestContactUsRequest();
-            $guest->name = $request->name;
-            $guest->email = $request->email;
-            $guest->messageBox = $request->messageBox;
-            $guest->save();
-            $name = $request->name;
-            $email = $request->email;
-            $messageBox = "<h4 style='color:red;'>Contact Us Request From User .</h4>" . "<hr>" . $request->messageBox;
-            $adminEmail = env('MAIL_ADMIN_EMAIL_ADDRESS');
-            $mail = new PHPMailer(true);
-            $mail->IsSMTP();
-            $mail->Host       = "smtp.gmail.com";
-            $mail->SMTPAuth   = TRUE;
-            $mail->Username   = env("MAIL_USERNAME");
-            $mail->Password   = env("MAIL_PASSWORD");
-            $mail->SMTPSecure = 'ssl';
-            $mail->Port       = 465;
-            $mail->setFrom($mail->Username);
-            $mail->AddReplyTo($email, $name);
-            $mail->AddAddress($adminEmail);
-            $mail->IsHTML(true);
-            $mail->Subject = "Conatact Us Request";
-            $mail->Body = $messageBox;
-            try {
-                $mail->send();
-            } catch (Exception $ex) {
-                return "Email Blocked Error Please Try Again Later ";
-            }
-            return redirect()->back()->with('errorMessage', 'Your Request Submited SuccessFully Our Team Will Contact You In 24 Hours')->with('className', 'success');
+        // if (Auth::user()) {
+        //     $user = new UserContactUsRequest();
+        //     $user->userId = Auth::user()->id;
+        //     $user->messageBox = $request->messageBox;
+        //     $user->save();
+        //     $name = $request->name;
+        //     $email = $request->email;
+        //     $messageBox = "<h4 style='color:red;'>Contact Us Request From User .</h4>" . "<hr>" . $request->messageBox;
+        //     $mail = new contactRequest();
+        //     $mail->name = $name;
+        //     $mail->email = $email;
+
+        //     Mail::to('khanhmdkhn13@gmail.com')->send(new contactRequest($mailData));
+        //     Mail::to('khanhmdkhn13@gmail.com')->send(new contactRequest($mailData));
+        //     $mail->Subject = "Conatact Us Request";
+        //     $mail->Body = $messageBox;
+        //     try {
+        //         $mail->send();
+        //     } catch (\Exception $ex) {
+        //         return "Email Blocked Error Please Try Again Later ";
+        //     }
+        //     return redirect()->back()->with('errorMessage', 'Your Request Submited SuccessFully Our Team Will Contact You In 24 Hours')->with('className', 'success');
+        // } else {
+        // $guest = new GuestContactUsRequest();
+        // $guest->name = $request->name;
+        // $guest->email = $request->email;
+        // $guest->messageBox = $request->messageBox;
+        // $guest->save();
+        $name = $request->name;
+        $email = $request->email;
+        $msgBox = $request->messageBox;
+        $admin = "admin@tempMail.com";
+        $mailData = [
+            "name" => $name,
+            "email" => $email,
+            "msgBox" => $msgBox,
+        ];
+        try {
+            Mail::to($admin)->send(new contctReqt($mailData));
+        } catch (\Exception $ex) {
+            return $ex;
+            // return "Email Blocked Error Please Try Again Later ";
         }
+        return redirect()->back()->with('errorMessage', 'Your Request Submited SuccessFully Our Team Will Contact You In 24 Hours')->with('className', 'success');
     }
 }
